c0f3135bf7f0d03ac6dbccaddebc9a60c6408919
Sergey Vorobev <vorobevse86@gmail.com>
2026-05-20 23:17:46 +0200

    feat: implement tag-based filtering in Portfolio

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,45 +1,58 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class Portfolio extends Component {
-  render() {
+const Portfolio = ({ data }) => {
+  const [activeTag, setActiveTag] = useState('All');
 
-    if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
-        var projectImage = process.env.PUBLIC_URL + '/images/portfolio/'+projects.image;
-        return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
-                  </div>
+  const tags = ['All', 'Backend', 'FullStack', 'Cloud', 'DataScience', 'ML', 'AI', 'IoT'];
+
+  const filteredProjects = activeTag === 'All' 
+    ? data?.projects || [] 
+    : (data?.projects || []).filter(project => project.tags && project.tags.includes(activeTag));
+
+  const projects = filteredProjects.map(function(project){
+    var projectImage = process.env.PUBLIC_URL + '/images/portfolio/' + project.image;
+    return <div key={project.title} className="columns portfolio-item">
+        <div className="item-wrap">
+        <a href={project.url} title={project.title}>
+            <img alt={project.title} src={projectImage} />
+            <div className="overlay">
+                <div className="portfolio-item-meta">
+                <h5>{project.title}</h5>
+                    <p>{project.category}</p>
                 </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>
+            </div>
+            <div className="link-icon"><i className="fa fa-link"></i></div>
+        </a>
+        </div>
+    </div>
+  });
+
+  return (
+    <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h1>Check Out Some of My Works.</h1>
+
+          <div className="portfolio-filters" style={{ marginBottom: '30px', textAlign: 'center' }}>
+            {tags.map(tag => (
+              <button 
+                key={tag}
+                className={`filter-btn ${activeTag === tag ? 'active' : ''}`}
+                onClick={() => setActiveTag(tag)}
+                style={{ margin: '5px', padding: '10px 20px', cursor: 'pointer' }}
+              >
+                {tag}
+              </button>
+            ))}
+          </div>
+
+          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+            {projects}
           </div>
         </div>
-      })
-    }
-
-    return (
-      <section id="portfolio">
-
-      <div className="row">
-
-         <div className="twelve columns collapsed">
-
-            <h1>Check Out Some of My Works.</h1>
-
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {projects}
-            </div>
-          </div>
       </div>
-   </section>
-    );
-  }
+    </section>
+  );
 }
 
 export default Portfolio;
